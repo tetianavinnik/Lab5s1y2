10a232db252c41bc315e5829aa802df085853cd6
tetianavinnik <tetiana.vinnik@ucu.edu.ua>
2022-10-13 13:41:39 +0300

    new

---
+++ src/test/java/flower_store/FlowerTest.java
@@ -0,0 +1,29 @@
+package flower_store;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FlowerTest {
+    private Flower flower;
+
+    @BeforeEach
+    public void init() {
+        flower = new Flower();
+    }
+
+    @Test
+    public void testPrice() {
+        int price = 20;
+        flower.setPrice(price);
+        assertEquals(price, flower.getPrice());
+    }
+
+    @Test
+    public void testColor() {
+        FlowerColor color = FlowerColor.RED;
+        flower.setColor(color);
+        assertEquals("#FF0000", flower.getColor());
+    }
+}
